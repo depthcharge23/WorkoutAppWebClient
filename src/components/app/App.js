a0da174f655389b3436e289414a95a47e35fcafc
Aaron Mathews <aamathews23@gmail.com>
2021-02-10 17:43:33 -0800

    Removed bootstrap from all the components and the npm packages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,11 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Import Bootstrap Components
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 // Import Custom Components
 import Header from "../header/Header";
 
 const App = () => {
   return (
-    <Container className="p-0 m-0" fluid>
-      <Row>
-        <Col>
-          <Header />
-        </Col>
-      </Row>
-    </Container>
+    <Header />
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,9 @@
 import React from "react";
 import "./Header.css";
 
-// Import Bootstrap Components
-import Navbar from "react-bootstrap/Navbar";
-
 const Header = () => {
     return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand>
-                <h1>The Workout App</h1>
-            </Navbar.Brand>
-        </Navbar>
+        <h1>The Workout App</h1>
     );
 }
 
