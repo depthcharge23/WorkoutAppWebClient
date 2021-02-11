266d9cb8967300321071424e022fad8ddee727ac
Aaron Mathews <aamathews23@gmail.com>
2021-02-11 15:40:50 -0800

    Added the regimen container to the app component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,10 +3,14 @@ import './App.css';
 
 // Import Custom Components
 import Header from "../header/Header";
+import RegimenContainer from "../regimen-container/RegimenContainer";
 
 const App = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <RegimenContainer />
+    </>
   );
 }
 
