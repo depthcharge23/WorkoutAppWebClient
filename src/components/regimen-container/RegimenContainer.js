000a01156c7347409717c07ba69212971364ae54
Aaron Mathews <aamathews23@gmail.com>
2021-02-11 15:44:00 -0800

    Added the regimen list component to the regimen container component

--- src/components/regimen-container/RegimenContainer.js
+++ src/components/regimen-container/RegimenContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './RegimenContainer.css';
 
+// Import Custom Components
+import RegimenList from "../regimen-list/RegimenList";
+
 const RegimenContainer = () => {
   return (
     <>
         <h2>Regimen</h2>
+        <RegimenList />
     </>
   );
 }
