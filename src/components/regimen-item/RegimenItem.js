57070afe8fae8b12902628bd490654c0e364e8a6
Aaron Mathews <aamathews23@gmail.com>
2021-02-11 15:45:42 -0800

    Created a basic regimen item component

---
+++ src/components/regimen-item/RegimenItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './RegimenItem.css';
+
+const RegimenItem = () => {
+    return (
+        <>
+            Hello world!
+        </>
+    );
+}
+
+export default RegimenItem;
