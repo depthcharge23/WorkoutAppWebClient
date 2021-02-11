5b2f371a0839abd88c2448edd9f5f9f37a1df5b2
Aaron Mathews <aamathews23@gmail.com>
2021-02-11 15:40:06 -0800

    Created the regimen container component

---
+++ src/components/regimen-container/RegimenContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './RegimenContainer.css';
+
+const RegimenContainer = () => {
+  return (
+    <>
+        <h2>Regimen</h2>
+    </>
+  );
+}
+
+export default RegimenContainer;
