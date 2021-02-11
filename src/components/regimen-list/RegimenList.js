6f930d8f33e3a58bc9d5f7fa48126b2545347a51
Aaron Mathews <aamathews23@gmail.com>
2021-02-11 15:43:16 -0800

    Created a basic regimen list component

---
+++ src/components/regimen-list/RegimenList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './RegimenList.css';
+
+const RegimenList = () => {
+    return (
+        <ul>
+            <li>Leg Day</li>
+            <li>CHEST!</li>
+            <li>Bis & Back</li>
+        </ul>
+    );
+}
+
+export default RegimenList;
