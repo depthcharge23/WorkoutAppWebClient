a1f8a2ce3d2d42f33dc9b82f19f1c3abd7b92c5c
Aaron Mathews <aamathews23@gmail.com>
2021-02-11 15:46:34 -0800

    Added the regimen item to the regimen list component

--- src/components/regimen-list/RegimenList.js
+++ src/components/regimen-list/RegimenList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './RegimenList.css';
 
+// Import Custom Components
+import RegimenItem from "../regimen-item/RegimenItem";
+
 const RegimenList = () => {
     return (
         <ul>
             <li>Leg Day</li>
             <li>CHEST!</li>
             <li>Bis & Back</li>
+            <li><RegimenItem /></li>
         </ul>
     );
 }
