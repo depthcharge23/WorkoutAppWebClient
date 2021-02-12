1e4051dadc0dd67e6e4f2d949359e3040e111c60
Aaron Mathews <aamathews23@gmail.com>
2021-02-11 16:03:27 -0800

    Created a basic regimen component

---
+++ src/components/regimen/Regimen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Regimen.css';
+
+class Regimen extends React.Component {
+    render () {
+        return (
+            <>
+                <h2>Regimen</h2>
+            </>
+        );
+    }
+}
+
+export default Regimen;
