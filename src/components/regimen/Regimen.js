d28272648e115071e82637490104a465cec52d01
Aaron Mathews <aamathews23@gmail.com>
2021-02-11 16:08:34 -0800

    Created the regimen form

--- src/components/regimen/Regimen.js
+++ src/components/regimen/Regimen.js
@@ -4,9 +4,16 @@ import './Regimen.css';
 class Regimen extends React.Component {
     render () {
         return (
-            <>
-                <h2>Regimen</h2>
-            </>
+            <form>
+                <label htmlFor="regimen-name">Regimen Name</label>
+                <input type="text" name="regimen-name" />
+
+                <label htmlFor="rest-between-workout">Rest Between Workout</label>
+                <input type="integer" name="rest-between-workout" />
+
+                <button>Back</button>
+                <button type="submit">Submit</button>
+            </form>
         );
     }
 }
