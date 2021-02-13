b1e3cd2fadb93c60c67b5fe4f5b4fbb2a85ef540
Aaron Mathews <aamathews23@gmail.com>
2021-02-13 10:18:29 -0800

    Added functionality to take user inputs from the form and submit them to the regime workout list

--- src/components/regimen-workout/RegimenWorkout.js
+++ src/components/regimen-workout/RegimenWorkout.js
@@ -5,26 +5,80 @@ class RegimenWorkout extends React.Component {
     constructor (props) {
         super(props);
 
+        this.state = {
+            "workoutName": "",
+            "sets": 0,
+            "reps": 0
+        };
+
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleOnWorkoutNameChange = this.handleOnWorkoutNameChange.bind(this);
+        this.handleOnSetsChange = this.handleOnSetsChange.bind(this);
+        this.handleOnRepsChange = this.handleOnRepsChange.bind(this);
     }
 
     handleSubmit (e) {
         e.preventDefault();
 
-        this.props.handleOnCreate("asdf", 1, 1);
+        this.props.handleOnCreate(this.state.workoutName, this.state.sets, this.state.reps);
+
+        this.setState({
+            "workoutName": "",
+            "sets": 0,
+            "reps": 0
+        });
+    }
+
+    handleOnWorkoutNameChange (e) {
+        const workoutName = e.target.value;
+
+        this.setState({
+            "workoutName": workoutName
+        });
+    }
+
+    handleOnSetsChange (e) {
+        const sets = e.target.value;
+
+        this.setState({
+            "sets": sets
+        });
+    }
+
+    handleOnRepsChange (e) {
+        const reps = e.target.value;
+
+        this.setState({
+            "reps": reps
+        });
     }
 
     render () {
         return (
             <form onSubmit={ this.handleSubmit }>
                 <label htmlFor="workout-name">Workout Name</label>
-                <input type="text" name="workout-name" />
+                <input
+                    type="text"
+                    name="workout-name"
+                    value={ this.state.workoutName }
+                    onChange={ this.handleOnWorkoutNameChange }
+                />
 
                 <label htmlFor="sets"># of Sets</label>
-                <input type="integer" name="sets" />
+                <input
+                    type="integer"
+                    name="sets"
+                    value={ this.state.sets }
+                    onChange={ this.handleOnSetsChange }
+                />
 
                 <label htmlFor="reps"># of Reps</label>
-                <input type="integer" name="reps" />
+                <input
+                    type="integer"
+                    name="reps"
+                    value={ this.state.reps }
+                    onChange={ this.handleOnRepsChange }
+                />
 
                 <button>Back</button>
                 <button type="submit">Submit</button>
