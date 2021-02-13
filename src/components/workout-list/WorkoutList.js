afbd8807ba2348609ceb25c8b6aeb90b490d644a
Aaron Mathews <aamathews23@gmail.com>
2021-02-12 16:10:42 -0800

    Created a basic workout list component

---
+++ src/components/workout-list/WorkoutList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./WorkoutList.css";
+
+const WorkoutList = () => {
+    return (
+        <>
+            <h2>Workouts</h2>
+            <ul>
+                <li>Push Ups</li>
+                <li>Squats</li>
+                <li>Lunges</li>
+            </ul>
+        </>
+    );
+};
+
+export default WorkoutList;
