b7f2167cef553f288065a1d80f8a6783d2fd1d8a
Aaron Mathews <aamathews23@gmail.com>
2021-02-13 09:25:45 -0800

    Added the regimen workout list component to the regimen workout container

--- src/components/regimen-workout-container/RegimenWorkoutContainer.js
+++ src/components/regimen-workout-container/RegimenWorkoutContainer.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./RegimenWorkoutContainer.css";
 
+// Import Custom Components
+import RegimenWorkoutList from "../regimen-workout-list/RegimenWorkoutList";
+
 class RegimenWorkoutContainer extends React.Component {
     render () {
         return (
             <>
                 <h2>Regimen Workouts</h2>
+                <RegimenWorkoutList />
             </>
         );
     }
