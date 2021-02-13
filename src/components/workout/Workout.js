4b80517cb03ed431e36c10516de2f827ce02a0d1
Aaron Mathews <aamathews23@gmail.com>
2021-02-12 21:59:35 -0800

    Added functionality to take form values and submit them to the workout list

--- src/components/workout/Workout.js
+++ src/components/workout/Workout.js
@@ -5,24 +5,57 @@ class Workout extends React.Component {
     constructor (props) {
         super(props);
 
+        this.state = {
+            "workoutName": "",
+            "workoutDescription": ""
+        };
+
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleOnWorkoutNameChange = this.handleOnWorkoutNameChange.bind(this);
+        this.handleOnWorkoutDescriptionChange = this.handleOnWorkoutDescriptionChange.bind(this);
     }
 
     handleSubmit (e) {
         e.preventDefault();
 
-        this.props.handleOnCreate("asdf", "asdf");
+        this.props.handleOnCreate(this.state.workoutName, this.state.workoutDescription);
+    }
+
+    handleOnWorkoutNameChange (e) {
+        const workoutName = e.target.value;
+
+        this.setState({
+            "workoutName": workoutName
+        });
+    }
+
+    handleOnWorkoutDescriptionChange (e) {
+        const workoutDescription = e.target.value;
+
+        this.setState({
+            "workoutDescription": workoutDescription
+        });
     }
 
     render () {
         return (
             <>
                 <form onSubmit={ this.handleSubmit }>
-                    <label>Workout Name</label>
-                    <input type="text" />
+                    <label htmlFor="workout-name">Workout Name</label>
+                    <input 
+                        type="text"
+                        name="workout-name"
+                        value={ this.state.workoutName }
+                        onChange={ this.handleOnWorkoutNameChange }
+                    />
 
-                    <label>Workout Description</label>
-                    <input type="text" />
+                    <label htmlFor="workout-description">Workout Description</label>
+                    <input
+                        type="text"
+                        name="workout-description"
+                        value={ this.state.workoutDescription }
+                        onChange={ this.handleOnWorkoutDescriptionChange }
+                    />
 
                     <button>Back</button>
                     <button type="submit">Submit</button>
