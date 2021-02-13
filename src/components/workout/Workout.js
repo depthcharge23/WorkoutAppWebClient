61bc69e23ccc58e7a8281d5c9a1c92bd38bfd692
Aaron Mathews <aamathews23@gmail.com>
2021-02-12 21:43:19 -0800

    Created a basic workout component

---
+++ src/components/workout/Workout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Workout.css";
+
+class Workout extends React.Component {
+    render () {
+        return (
+            <>
+                Workout
+            </>
+        );
+    }
+}
+
+export default Workout;
