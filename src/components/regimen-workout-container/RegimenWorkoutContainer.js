7a0f48c474573257a0fd01e73626ef9790d8aab3
Aaron Mathews <aamathews23@gmail.com>
2021-02-13 09:21:10 -0800

    Created a basic regimen workout container component

---
+++ src/components/regimen-workout-container/RegimenWorkoutContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./RegimenWorkoutContainer.css";
+
+class RegimenWorkoutContainer extends React.Component {
+    render () {
+        return (
+            <>
+                <h2>Regimen Workouts</h2>
+            </>
+        );
+    }
+}
+
+export default RegimenWorkoutContainer;
