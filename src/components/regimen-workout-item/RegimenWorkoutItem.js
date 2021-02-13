a221c7ab3d336d055cf05f7ba8568f69d938dfe7
Aaron Mathews <aamathews23@gmail.com>
2021-02-13 09:30:19 -0800

    Created the regimen workout item component

---
+++ src/components/regimen-workout-item/RegimenWorkoutItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./RegimenWorkoutItem.css";
+
+const RegimenWorkoutItem = () => {
+    return (
+        <>
+            Hello world
+        </>
+    );
+}
+
+export default RegimenWorkoutItem;
