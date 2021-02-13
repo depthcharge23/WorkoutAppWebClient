4cd161dadfcddd46eeec708cbd6d20603990d12b
Aaron Mathews <aamathews23@gmail.com>
2021-02-13 09:23:46 -0800

    Created a basic regimen workout list component

---
+++ src/components/regimen-workout-list/RegimenWorkoutList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./RegimenWorkoutList.css";
+
+const RegimenWorkoutList = () => {
+    return (
+        <>
+            <ul>
+                <li>Push Ups</li>
+                <li>Sit Ups</li>
+                <li>Crunches</li>
+            </ul>
+        </>
+    )
+}
+
+export default RegimenWorkoutList;
