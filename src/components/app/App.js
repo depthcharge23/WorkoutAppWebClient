04e481b33bf3d58c9b87143cd630c547b67d9fce
Aaron Mathews <aamathews23@gmail.com>
2021-02-12 16:07:38 -0800

    Added the workout container component to the main app component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,12 +4,14 @@ import './App.css';
 // Import Custom Components
 import Header from "../header/Header";
 import RegimenContainer from "../regimen-container/RegimenContainer";
+import WorkoutContainer from "../workout-container/WorkoutContainer";
 
 const App = () => {
   return (
     <>
       <Header />
       <RegimenContainer />
+      <WorkoutContainer />
     </>
   );
 }
