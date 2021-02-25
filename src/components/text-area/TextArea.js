e3b80084fcc129195cf2dea20ea7187b6a4cde13
Aaron Mathews <aamathews23@gmail.com>
2021-02-24 20:08:28 -0800

    Created a basic text area component

---
+++ src/components/text-area/TextArea.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./TextArea.css";
+
+const TextArea = () => {
+    return (
+        <>
+            <textarea>
+                
+            </textarea>
+        </>
+    );
+};
+
+export default TextArea;
