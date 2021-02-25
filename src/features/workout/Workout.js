b71109a7ae1d4cc85df2dc69d8ca06fb843e52c7
Aaron Mathews <aamathews23@gmail.com>
2021-02-24 20:21:29 -0800

    Added the textarea component to the workout component

--- src/features/workout/Workout.js
+++ src/features/workout/Workout.js
@@ -15,35 +15,18 @@ class Workout extends React.Component {
         this.state = {
             "workoutName": this.props.workout && this.props.workout.workoutName ? this.props.workout.workoutName : "",
             "workoutDescription": this.props.workout && this.props.workout.workoutDescription ? this.props.workout.workoutDescription : "",
-            "workoutDescriptionError": "",
             "isError": false
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleOnWorkoutDescriptionChange = this.handleOnWorkoutDescriptionChange.bind(this);
-
         this.setWorkoutName = this.setWorkoutName.bind(this);
+        this.setWorkoutDescription = this.setWorkoutDescription.bind(this);
     }
 
     handleSubmit (e) {
         e.preventDefault();
 
-        let hasError = false;
-
-        let error = WorkoutModel.validateWorkoutDescription(this.state.workoutDescription);
-
-        if (error) {
-            const element = document.querySelector(`input[name="workout-description"]`);
-            element.classList.add("error-input");
-
-            hasError = true;
-
-            this.setState({
-                "workoutDescriptionError": error
-            });
-        }
-
-        if (!hasError) {
+        if (this.state.workoutName && this.state.workoutDescription && !this.state.isError) {
             this.props.handleOnCreate(this.state.workoutName, this.state.workoutDescription);
 
             this.setState({
@@ -60,19 +43,10 @@ class Workout extends React.Component {
         });
     }
 
-    handleOnWorkoutDescriptionChange (e) {
-        const workoutDescription = e.target.value;
-        let error = WorkoutModel.validateWorkoutDescription(workoutDescription);
-
-        if (error) {
-            e.target.classList.add("error-input");
-        } else {
-            e.target.classList.remove("error-input");
-        }
-
+    setWorkoutDescription (workoutDescription, isError) {
         this.setState({
             "workoutDescription": workoutDescription,
-            "workoutDescriptionError": error
+            "isError": isError
         });
     }
 
@@ -93,22 +67,16 @@ class Workout extends React.Component {
                         callback={ this.setWorkoutName }
                     /><br />
 
-                    <label className="label" htmlFor="workout-description">
-                        Workout Description
-                    </label><br />
-
-                    <p className="error">{ this.state.workoutDescriptionError }</p>
-
-                    <textarea
-                        className="textarea"
-                        name="workout-description"
+                    <TextArea 
+                        textAreaName="workout-description"
+                        textAreaDisplay="Workout Description"
                         rows="5"
                         cols="100"
                         value={ this.state.workoutDescription }
-                        onChange={ this.handleOnWorkoutDescriptionChange }
-                    ></textarea><br />
-
-                    <TextArea /><br />
+                        isAsync={ false }
+                        validate={ WorkoutModel.validateWorkoutDescription }
+                        callback={ this.setWorkoutDescription }
+                    /><br />
 
                     <button className="back-button" onClick={ this.props.showWorkoutList }>Back</button>
                     { submitButton }
